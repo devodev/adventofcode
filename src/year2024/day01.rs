c6206864cca0ecca59c68f2fddff24aaf92ff143
devodev <abalexandrebarone@gmail.com>
2024-12-01 16:03:10 -0500

    Add 2024 day01 part1 solution

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -1,4 +1,7 @@
-use tracing::info;
+use std::collections::BinaryHeap;
+
+use adventofcode::split2;
+use anyhow::{anyhow, Result};
 
 #[derive(Debug, clap::Args)]
 pub struct Args {
@@ -8,19 +11,66 @@ pub struct Args {
 
 #[derive(Debug, clap::Subcommand)]
 pub enum Commands {
-    /// Advent of Code 2024 - Day 01 - Historian Hysteria.
-    HistorianHysteria,
+    /// Part 1 (binary heap variant).
+    ///
+    /// Uses a BinaryHeap to accumulate column values and have them sorted on insert.
+    Part1BinaryHeap,
+
+    /// Part 1 (sort variant).
+    ///
+    /// Uses a Vec to accumulate column values and sorts them after insert.
+    Part1Sort,
+
+    /// Part 2.
+    Part2,
 }
 
 impl Args {
-    pub fn run(self) -> anyhow::Result<()> {
+    pub fn run(self, input: String) -> Result<()> {
         match self.command {
-            Commands::HistorianHysteria => hystorian_hysteria(),
+            Commands::Part1BinaryHeap => part1_binary_heap(input),
+            Commands::Part1Sort => part1_sort(input),
+            Commands::Part2 => part2(input),
         }
     }
 }
 
-fn hystorian_hysteria() -> anyhow::Result<()> {
-    info!("Solving: Advent of Code 2024 - Day 01 - Historian Hysteria");
+fn part1_binary_heap(input: String) -> Result<()> {
+    let mut col1 = BinaryHeap::new();
+    let mut col2 = BinaryHeap::new();
+    for line in input.lines() {
+        let (left, right) = split2::<u32>(line)?;
+        col1.push(left);
+        col2.push(right);
+    }
+
+    let col1 = col1.into_sorted_vec();
+    let col2 = col2.into_sorted_vec();
+
+    let total_distance: u32 = col1.iter().zip(col2).map(|(left, right)| left.abs_diff(right)).sum();
+    println!("{total_distance}");
+
     Ok(())
+}
+
+fn part1_sort(input: String) -> Result<()> {
+    let mut col1 = Vec::new();
+    let mut col2 = Vec::new();
+    for line in input.lines() {
+        let (left, right) = split2::<u32>(line)?;
+        col1.push(left);
+        col2.push(right);
+    }
+
+    col1.sort();
+    col2.sort();
+
+    let total_distance: u32 = col1.iter().zip(col2).map(|(left, right)| left.abs_diff(right)).sum();
+    println!("{total_distance}");
+
+    Ok(())
+}
+
+fn part2(_input: String) -> Result<()> {
+    Err(anyhow!("not implemented"))
 }
--- src/year2024/mod.rs
+++ src/year2024/mod.rs
@@ -1,5 +1,7 @@
 mod day01;
 
+use anyhow::Result;
+
 #[derive(Debug, clap::Args)]
 pub struct Args {
     #[command(subcommand)]
@@ -8,15 +10,15 @@ pub struct Args {
 
 #[derive(Debug, clap::Subcommand)]
 pub enum Commands {
-    /// Advent of Code 2024 - Day 01.
-    #[clap(visible_aliases = &["01", "1"])]
+    /// Advent of Code 2024 - Day 01 - Historian Hysteria.
+    #[command(visible_aliases = &["day1", "01", "1"])]
     Day01(day01::Args),
 }
 
 impl Args {
-    pub fn run(self) -> anyhow::Result<()> {
+    pub fn run(self, input: String) -> Result<()> {
         match self.command {
-            Commands::Day01(args) => args.run(),
+            Commands::Day01(args) => args.run(input),
         }
     }
 }
